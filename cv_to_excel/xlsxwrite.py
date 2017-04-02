c1332c4f74827dfc7289e7e4c82938cbf61fd26b
zhang-alex <alexzhang02030203@gmail.com>
2017-04-02 12:22:14 -0500

    i lied... This is the ACTUAL last commit for a while. Everything's been put into 1 method and a slight modification was made because of a miscommunication of method argument formats.

--- cv_to_excel/xlsxwrite.py
+++ cv_to_excel/xlsxwrite.py
@@ -1,13 +1,12 @@
-##############################################################################
-#
-# A simple example of some of the features of the XlsxWriter Python module.
-#
-# Copyright 2013-2016, John McNamara, jmcnamara@cpan.org
-#
+# import the python math module necessary for certain arithmetic computations
 import math
+
+# an ordered list of characters from the English alphabet
 base26 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-def num2char(num) :
+# this method will convert a number to its corresponding excel column value.
+# It's essentially a base-26 conversion, but with a few modifications
+def num2col(num) :
     i = 1
     counter = 1
     while num >= i:
@@ -30,6 +29,9 @@ def num2char(num) :
 
     return cell + ":" + cell
 
+# When given a list of cells to merge, xlsxwriter requires that I provide the top-left cell and the bottom-right cell.
+# This method is used to compute those and works by sorting the cell values based on their columns and rows.
+# The row is multiplied by the maximum number of columns
 def char2num(char) :
 
     # xlsxwriter supports excel's worksheet limits of 1,048,576 rows by 16,384 columns.
@@ -48,7 +50,7 @@ def char2num(char) :
         counter += 1
 
     #max excel length
-    value += (2**20) * int(rows)
+    value += (2**14) * int(rows)
 
     def f(x):
         return {
@@ -109,7 +111,7 @@ def out(rowheights, columnwidths, values) :
         worksheet.set_row(i, rowheights[i])
 
     for j in range(len(columnwidths)) :
-        worksheet.set_column(num2char(j+1), columnwidths[j])
+        worksheet.set_column(num2col(j+1), columnwidths[j])
 
 
 
@@ -155,18 +157,10 @@ def out(rowheights, columnwidths, values) :
             worksheet.merge_range(asdf, values[l][len(values[l])-1], default_format)
 
 
-
-
-
-
-
-
-
-
-##col = [20,25,30,35,40,45,50,55,60,65,70,75]
-##row = [30,40,50,60,70,80,90,100,110,120,130]
-##hello = [('A1','B2','A3','C4','A4','C2','B1','C3','B3','C1','B4','A2',"First entry"), ('D5',"Second entry"), ('E7','F8','E8','F7',"second merge")]
-##out(row, col, hello)
+col = [20,25,30,35,40,45,50,55,60,65,70,75]
+row = [30,40,50,60,70,80,90,100,110,120,130]
+hello = [('A1','B2','A3','C4','A4','C2','B1','C3','B3','C1','B4','A2',"First entry"), ('D5',"Second entry"), ('E7','F8','E8','F7',"second merge")]
+out(row, col, hello)
 
 
 
