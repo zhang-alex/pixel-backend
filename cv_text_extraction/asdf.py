8b6e56d165608dd50b91dc68368a081add12ec1b
zhang-alex <alexzhang02030203@gmail.com>
2017-04-03 22:17:23 -0500

    did some experiments with OpenCV Canny Edge Detection.

--- cv_text_extraction/asdf.py
+++ cv_text_extraction/asdf.py
@@ -1,20 +1,16 @@
-# import the opencv-python module
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-# equivalent to image = cv2.imread(digits.png, cv2.IMREAD_GRAYSCALE)
-image = cv2.imread('digits.png',0)
+file_path = ""
 
-# display window with title and image
-cv2.imwrite('title of image',image)
+for i in range(2000) :
 
+    img = cv2.imread('tesla.png',0)
+    edges = cv2.Canny(img,(i/20)**2,1.5*((i/20)**2))
 
+    file_path = 'canny_tesla_%d.png' % i
 
+    cv2.imwrite(file_path,edges)
 
-
-if y1 > x1 :
-    x2 = 100
-    y2 = (100/x1)*y1
-
-else :
-    y2 = 100
-    x1 = (100/y1)*x1
+    print i
